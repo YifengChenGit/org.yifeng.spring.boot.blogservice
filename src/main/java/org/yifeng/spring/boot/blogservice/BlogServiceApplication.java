85e5bd062f5128b207976cbe1087e87a5c588aa3
YifengChenGit <ycpostm@gmail.com>
2018-02-24 13:41:38 -0500

    generated a brand new spring boot project with DevTools, Web, JPA, MySQL dependencies

---
+++ src/main/java/org/yifeng/spring/boot/blogservice/BlogServiceApplication.java
@@ -0,0 +1,12 @@
+package org.yifeng.spring.boot.blogservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogServiceApplication.class, args);
+	}
+}
